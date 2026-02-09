7905fbc8cde57d11ccd32ff05a015b800099bfc1
Muhammad Huzaifa Hameed <etxhuzyyy@gmail.com>
2026-02-09 15:01:17 -0400

    Persistent Key added

--- app/crypto.py
+++ app/crypto.py
@@ -1,7 +1,20 @@
+import os
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-KEY = get_random_bytes(16)  # AES-128
+KEY_FILE = "data/secret.key"
+
+def load_key():
+    if os.path.exists(KEY_FILE):
+        with open(KEY_FILE, "rb") as f:
+            return f.read()
+    else:
+        key = get_random_bytes(16)
+        with open(KEY_FILE, "wb") as f:
+            f.write(key)
+        return key
+
+KEY = load_key()
 
 def encrypt(data: bytes):
     cipher = AES.new(KEY, AES.MODE_GCM)
