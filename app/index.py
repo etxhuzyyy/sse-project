9a6b0095f229c079fa2b84dcdca6db31ade4cf42
aitezaz.imtiaz <aitezaz.imtiaz@unb.ca>
2026-03-23 23:39:03 -0300

    fixed trapdoor

--- app/index.py
+++ app/index.py
@@ -1,18 +1,20 @@
 import json
+import hmac
 import hashlib
+import re
+from app.crypto import KEY  # if this gives import error, try: from crypto import KEY
 
 DOC_KEYWORDS_FILE = "data/doc_keywords.json"
-doc_keywords = {}
-
 INDEX_FILE = "data/index.json"
 
+doc_keywords = {}
 index = {}
 
 def tokenize(text):
-    return text.lower().split()
+    return re.findall(r"\b\w+\b", text.lower())
 
 def trapdoor(word):
-    return hashlib.sha256(word.encode()).hexdigest()
+    return hmac.new(KEY, word.encode(), hashlib.sha256).hexdigest()
 
 def add_document(filename, content):
     words = set(tokenize(content))
@@ -20,24 +22,33 @@ def add_document(filename, content):
 
     for w in words:
         key = trapdoor(w)
-        index.setdefault(key, []).append(filename)
-
+        if filename not in index.setdefault(key, []):
+            index[key].append(filename)
 
 def save():
     with open(INDEX_FILE, "w") as f:
-        json.dump(index, f)
+        json.dump(index, f, indent=2)
+
     with open(DOC_KEYWORDS_FILE, "w") as f:
-        json.dump(doc_keywords, f)
+        json.dump(doc_keywords, f, indent=2)
 
 def load():
     global index, doc_keywords
+
     try:
-        with open(INDEX_FILE) as f:
+        with open(INDEX_FILE, "r") as f:
             index = json.load(f)
-        with open(DOC_KEYWORDS_FILE) as f:
-            doc_keywords = json.load(f)
-    except:
+    except FileNotFoundError:
         index = {}
+    except json.JSONDecodeError:
+        index = {}
+
+    try:
+        with open(DOC_KEYWORDS_FILE, "r") as f:
+            doc_keywords = json.load(f)
+    except FileNotFoundError:
+        doc_keywords = {}
+    except json.JSONDecodeError:
         doc_keywords = {}
 
 def search(word):
